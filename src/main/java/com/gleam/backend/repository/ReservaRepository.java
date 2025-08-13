5402e9a0a8ba540fa32dab0170686f87c6f3eeaa
VINÍCIUS GOMES SENS <vinicius_sens@estudante.sesisenai.org.br>
2025-08-13 15:08:15 -0300

    rollback

--- src/main/java/com/gleam/backend/repository/ReservaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gleam.backend.repository;
-
-import com.gleam.backend.model.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
