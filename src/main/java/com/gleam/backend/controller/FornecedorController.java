45c92ac1e9a9ea78b18c540638ac5721413c7cca
VINÍCIUS GOMES SENS <vinicius_sens@estudante.sesisenai.org.br>
2025-08-15 16:36:35 -0300

    update backend files

--- src/main/java/com/gleam/backend/controller/FornecedorController.java
+++ src/main/java/com/gleam/backend/controller/FornecedorController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/fornecedores")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class FornecedorController {
 
     @Autowired
